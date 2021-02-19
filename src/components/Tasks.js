328c0de57271ff0fd999a93106bdaa608c86711c
mohit.kumar <mohit.kumar@pantelwar.com>
2021-02-19 13:38:44 +0530

    Sorting of tasks in alphabetically order added

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,12 +2,12 @@ import Task from "./Task"
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
-        <>
-            {tasks.map((task, index) => (
+        <div className="tasks-wrapper">
+            {tasks.sort((a,b) => a.text.localeCompare(b.text, 'en', {'sensitivity': 'base'})).map((task, index) => (
                 <Task key={index} task={task} onDelete=
                 {onDelete} onToggle={onToggle} />
             ))}
-        </>
+        </div>
     )
 }
 
